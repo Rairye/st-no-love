a9581cc624d95c2318efd404f271dd322adf620d
Rairye <37942271+Rairye@users.noreply.github.com>
2021-10-28 06:32:50 +0900

    Update de-script.js

--- de-script.js
+++ de-script.js
@@ -16,7 +16,7 @@ limitations under the License.
 
 function stripNonAlpha(string) {
 	
-	var abcSet = new Set(["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]);
+	var abcSet = new Set(["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "\\"]);
 	var i = 0;
 	
 	while (i < string.length) {
