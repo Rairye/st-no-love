5cc3b492f65cbfa05b0369a5c3fd7ced3105641e
Rairye <37942271+Rairye@users.noreply.github.com>
2021-10-28 12:34:16 +0900

    Update st-no-love.js

--- st-no-love.js
+++ st-no-love.js
@@ -88,6 +88,6 @@ function escapeScriptTags(inputText) {
 		return (result + inputText.substring(lastOpen));
 	}
 			
-	return (inputLength == lastReplacement ? result : result+inputText.substring(lastReplacement+1));
+	return (inputLength - 1 == lastReplacement ? result : result+inputText.substring(lastReplacement+1));
 
 }
